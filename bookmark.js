1ff334abb42c2c3ccf0ca74cdfff5e310b0f081e
rudwnok123 <rudwnok123@naver.com>
2018-10-24 00:46:25 +0900

    first readme

---
+++ bookmark.js
@@ -0,0 +1,14 @@
+var bookmarkLayer = document.createElement('div');
+bookmarkLayer.className = 'popup_layer';
+
+const popup_setting = {
+  'position': {
+    top: 0,
+    'right':0
+  },
+  'rect': {
+    'width': 500,
+    'height': 700
+  }
+}
+document.body.appendChild(bookmarkLayer);
--- template.js
+++
@@ -1,9 +0,0 @@
-var data = {  title : "hello",
-              content : "lorem dkfief",
-              price : 2000
-           };
-var html = "<li><h4>{title}</h4><p>{content}</p><div>{price}</div></li>";
-
-html.replace("{title}", data.title)
-    .replace("{content}", data.content)
-    .replace("{price}", data.price)
